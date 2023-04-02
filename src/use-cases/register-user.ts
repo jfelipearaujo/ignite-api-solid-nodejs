723506204f639890ba3cf55015a6c7a739ce245c
José Felipe Blum de Araújo <j.felipe.araujo@outlook.com>
2023-04-02 14:30:03 -0300

    adding register user usecase

---
+++ src/use-cases/register-user.ts
@@ -0,0 +1,30 @@
+import { prisma } from '@/lib/prisma';
+import { hash } from 'bcryptjs';
+
+interface RegisterUser {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export async function registerUserUseCase ({ name, email, password }: RegisterUser) {
+    const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+            email,
+        }
+    });
+
+    if(userWithSameEmail) {
+        throw new Error('Email already exists');
+    }
+
+    const password_hash = await hash(password, 6);
+
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password_hash
+        }
+    });
+}
