bd206a204a6f3da3a3bf664e5153d20178f62226
José Felipe Blum de Araújo <j.felipe.araujo@outlook.com>
2023-04-02 14:03:32 -0300

    add route to register users

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
 import fastify from 'fastify';
-import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
+import { prisma } from './lib/prisma';
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
+app.post('/users', async (request, reply) => {
+    const registerUserSchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password : z.string().min(6)
+    });
 
-prisma.user.create({
-    data: {
-        name: 'John Doe',
-        email: 'john.doe@example.com'
-    }
+    const { name, email, password } = registerUserSchema.parse(request.body);
+
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password_hash: password
+        }
+    });
+
+    return reply.status(201).send();
 });
--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,5 @@ app.listen({
     host: '0.0.0.0',
     port: env.PORT
 }).then(() => {
-    console.log(`listening on port ${env.PORT}`);
+    console.log(`🚀 listening on port ${env.PORT}`);
 });
