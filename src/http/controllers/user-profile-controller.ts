3a3f6541a1d08047e6117efe950b2fe19c3ed324
José Felipe Blum de Araújo <j.felipe.araujo@outlook.com>
2023-04-04 22:11:10 -0300

    add auth middleware

--- src/http/controllers/user-profile-controller.ts
+++ src/http/controllers/user-profile-controller.ts
@@ -1,11 +1,20 @@
+import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-usecase";
 import { FastifyRequest, FastifyReply } from "fastify";
 
 export async function userProfile(
     request: FastifyRequest,
     reply: FastifyReply,
 ) {
-    await request.jwtVerify();
-    console.log(request.user.sub);
+    const getUserProfile = makeGetUserProfileUseCase();
 
-    return reply.status(200).send();
+    const { user } = await getUserProfile.execute({
+        userId: request.user.sub,
+    });
+
+    return reply.status(200).send({
+        user: {
+            ...user,
+            password_hash: undefined,
+        },
+    });
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,11 +2,18 @@ import { FastifyInstance } from "fastify";
 import { registerUser } from "./controllers/register-user-controller";
 import { authenticateUser } from "./controllers/authenticate-user-controller";
 import { userProfile } from "./controllers/user-profile-controller";
+import { verifyJwt } from "./middlewares/verify-jwt";
 
 export async function appRoutes(app: FastifyInstance) {
     app.post("/users", registerUser);
     app.post("/sessions", authenticateUser);
 
     /** Only for authenticated users */
-    app.get("/me", userProfile);
+    app.get(
+        "/me",
+        {
+            onRequest: [verifyJwt],
+        },
+        userProfile,
+    );
 }
