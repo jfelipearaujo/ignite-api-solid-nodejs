257f2b050521ed9e68c42168befd4b9f172163ef
José Felipe Blum de Araújo <j.felipe.araujo@outlook.com>
2023-04-04 23:03:23 -0300

    add register user controller test

--- src/http/controllers/register-user-controller.spec.ts
+++ src/http/controllers/register-user-controller.spec.ts
@@ -1,3 +1,27 @@
-import { test } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
-test("ok", () => {});
+import { app } from "@/app";
+import request from "supertest";
+
+describe("RegisterUserController", () => {
+    beforeAll(async () => {
+        await app.ready();
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
+
+    it("should be able to register a user", async () => {
+        // Arrange
+        // Act
+        const response = await request(app.server).post("/users").send({
+            name: "John Doe",
+            email: "john.doe@email.com",
+            password: "123456",
+        });
+
+        // Assert
+        expect(response.status).toEqual(201);
+    });
+});
