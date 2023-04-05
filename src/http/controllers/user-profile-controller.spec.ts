f2961fe6ff61030d1b1ae6de9a391df973b89d0f
José Felipe Blum de Araújo <j.felipe.araujo@outlook.com>
2023-04-04 23:12:30 -0300

    add get user profile controller test

---
+++ src/http/controllers/user-profile-controller.spec.ts
@@ -0,0 +1,45 @@
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+
+import { app } from "@/app";
+import request from "supertest";
+
+describe("UserProfileController", () => {
+    beforeAll(async () => {
+        await app.ready();
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
+
+    it("should be able to get an user's profile", async () => {
+        // Arrange
+        await request(app.server).post("/users").send({
+            name: "John Doe",
+            email: "john.doe@email.com",
+            password: "123456",
+        });
+
+        const authResponse = await request(app.server).post("/sessions").send({
+            email: "john.doe@email.com",
+            password: "123456",
+        });
+
+        const { token } = authResponse.body;
+
+        // Act
+        const response = await request(app.server)
+            .get("/me")
+            .set("Authorization", `Bearer ${token}`)
+            .send();
+
+        // Assert
+        expect(response.status).toEqual(200);
+        expect(response.body.user).toEqual(
+            expect.objectContaining({
+                name: "John Doe",
+                email: "john.doe@email.com",
+            }),
+        );
+    });
+});
