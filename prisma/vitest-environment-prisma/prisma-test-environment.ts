39e4bba503ab551b986330c3579abf7eeea3dba9
José Felipe Blum de Araújo <j.felipe.araujo@outlook.com>
2023-04-04 22:55:56 -0300

    setup prisma e2e enviroment

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,42 @@
+import "dotenv/config";
+
+import { randomUUID } from "node:crypto";
+import { execSync } from "node:child_process";
 import { Environment } from "vitest";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+function generateDatabaseUrl(schema: string) {
+    if (!process.env.DATABASE_URL) {
+        throw new Error("DATABASE_URL environment variable is not set");
+    }
+
+    const url = new URL(process.env.DATABASE_URL);
+
+    // update the database url with the given schema
+    url.searchParams.set("schema", schema);
+
+    return url.toString();
+}
 
 export default <Environment>{
     name: "prisma",
     async setup() {
-        console.log("setup prisma");
+        const schema = randomUUID();
+        const databaseUrl = generateDatabaseUrl(schema);
+
+        process.env.DATABASE_URL = databaseUrl;
+
+        execSync("npx prisma migrate deploy");
 
         return {
             async teardown() {
-                console.log("teardown prisma");
+                await prisma.$executeRawUnsafe(
+                    `DROP SCHEMA IF EXISTS "${schema}" CASCADE;`,
+                );
+
+                await prisma.$disconnect();
             },
         };
     },
