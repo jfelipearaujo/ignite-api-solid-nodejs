3c30c5d60a248b743a037fe7b7f68da83d46b31c
José Felipe Blum de Araújo <j.felipe.araujo@outlook.com>
2023-04-08 17:02:51 -0300

    fix env vars

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 const envSchema = z.object({
     NODE_ENV: z.enum(["dev", "test", "prod"]),
-    PORT: z.coerce.number(),
+    PORT: z.coerce.number().default(3333),
     JWT_SECRET: z.string(),
 });
 
