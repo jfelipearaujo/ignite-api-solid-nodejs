57de56e199509de19dbd74a4087ba3a2617c7fdd
José Felipe Blum de Araújo <j.felipe.araujo@outlook.com>
2023-04-08 15:53:01 -0300

    implementing refresh token

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,11 +3,14 @@ import { FastifyInstance } from "fastify";
 import { authenticate } from "./authenticate";
 import { create } from "./create";
 import { profile } from "./profile";
+import { refresh } from "./refresh";
 import { verifyJwt } from "../../middlewares/verify-jwt";
 
 export async function usersRoutes(app: FastifyInstance) {
     app.post("/users", create);
     app.post("/sessions", authenticate);
+
+    app.patch("/token/refresh", refresh);
 
     /** Only for authenticated users */
     app.get(
